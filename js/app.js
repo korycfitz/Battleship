e926a44ee0aab06014d125f785f04cbd4f86b10c
korycfitz <korycfitz@gmail.com>
2023-03-31 11:28:58 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+//Initial Data Structure
+//Board: 10x10   Grid: 10 arrays of lengths 10, nested in an outter array
+//ship location: object (   keys = shipName,    value = [[row,col], [row,col]  )
+
