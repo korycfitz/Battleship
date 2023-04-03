cfeb54b091aa6edd83184857d7a0d1dc6256a282
korycfitz <korycfitz@gmail.com>
2023-04-03 11:13:45 -0400

    make progress on rebuilding out logic of game

--- js/app.js
+++ js/app.js
@@ -250,30 +250,33 @@
 
 //-------------------------------------------------------------------------------------
 // 1) Define the required variables used to track the state of the game
-let width, height, board, turn, currentShipNum, currentShip, placedShips, ships, allShipsPlaced, ship1, ship2, ship3, ship4, ship5, gameMode //game status will display false before in 'game' mode and true after
+let width, height, board, turn, currentShipNum, currentShip, placedShips, ships, allShipsPlaced, ship1, ship2, ship3, ship4, ship5, shipsHidden//game status will display false before in 'game' mode and true after
 /*---------------------------- Variables (state) ----------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 // 2) Store cached element references
 const messageEl = document.getElementById("message1");
 const squareEls = document.querySelectorAll(".sqr1");
-const btn1 = document.getElementById("ship1")
-const btn2 = document.getElementById("ship2")
-const btn3 = document.getElementById("ship3")
-const btn4 = document.getElementById("ship4")
-const btn5 = document.getElementById("ship5")
+const playGame = document.getElementById("play-game")
 // const resetBtnEl = document.querySelector('#reset-button');
 /*----------------------------- Event Listeners -----------------------------*/
-btn1.addEventListener('click', handleBtnClick)
-btn2.addEventListener('click', handleBtnClick)
-btn3.addEventListener('click', handleBtnClick)
-btn4.addEventListener('click', handleBtnClick)
-btn5.addEventListener('click', handleBtnClick)
+playGame.addEventListener('click', handleBtnClick)
 // resetBtnEl.onclick = function(){
 //   init();
 // }
 /*-------------------------------- Functions --------------------------------*/
+function handleBtnClick(){
+  init();
+//will need to remove it after all of the ships are placed for both players
+//then we will add a new event listener
+  squareEls.forEach(ele => {
+    ele.addEventListener("click", handleSqClick);
+  })
+}
+
+
 function init(){
+  //build out board using document.createElement later on
   board = [
     [null, null, null, null, null, null, null, null, null, null], 
     [null, null, null, null, null, null, null, null, null, null], 
@@ -309,11 +312,10 @@ function init(){
     ship4: [],
     ship5: [],
   }
-  player = 1;
   turn = 1;
-  boardFull = false;
+  allShipsPlaced = false;
+  shipsHidden = false;
   winner = false;
-  gameMode = false;
   render();
 }
 
@@ -322,15 +324,17 @@ function render(){
   updateBoard();
   //updateBoard will only work for the game before it is in the "play" state because we will need to update the board differently, to display hits and misses. I need to figure out how to handle a different board state.
   updateMessage();
+  //add event listeners to squares
 }
 
 function updateBoard(){
-  if (!gameMode) {
+  //if all ships aren't placed, update board to display ships that are being placed 
+  if (!allShipsPlaced) {
     let i = 0
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++){
         if (board[row][col] === 1) {
-          squareEls[i].textContent = "S1" 
+          squareEls[i].textContent = "S1"
         } else if(board[row][col] === 2) {
           squareEls[i].textContent = "S2"
         } else if(board[row][col] === 3) {
@@ -346,57 +350,25 @@ function updateBoard(){
       }
     }
   } else {
-    //it is in game mode(not placing ships)// if ship gets guessed, update square, based on if it is a hit or missed
+    //all ships are place
+    //check if hidden
+    
+    //check if ai placed ships yet
+    //if these are both true, update board differently according to game play
   }
 }
 function updateMessage(){
   //render message based on current game state
-  // if (winner === false && boardFull === false && ship === 1 && turn === 1) {
-  //   messageEl.textContent = `Player 1 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 2 && turn === 1) {
-  //   messageEl.textContent = `Player 1 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 3 && turn === 1) {
-  //   messageEl.textContent = `Player 1 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 4 && turn === 1) {
-  //   messageEl.textContent = `Player 1 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 5 && turn === 1) {
-  //   messageEl.textContent = `Player 1 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 1 && turn === -1) {
-  //   messageEl.textContent = `Player 2 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 2 && turn === -1) {
-  //   messageEl.textContent = `Player 2 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 3 && turn === -1) {
-  //   messageEl.textContent = `Player 2 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 4 && turn === -1) {
-  //   messageEl.textContent = `Player 2 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === false && ship === 5 && turn === -1) {
-  //   messageEl.textContent = `Player 2 Please place ship ${ship}!`
-  // } else if (winner === false && boardFull === true && turn === 1) {
-  //   messageEl.textContent = `Player 1 Place the /'Hide Ships' Button to hide your ships!`
-  // } else if (winner === false && boardFull === true && turn === -1) {
-  //   messageEl.textContent = `Player 2 Place the /'Hide Ships' Button to hide your ships!`
-  // } else if (winner === true && turn === 1) {
+  if (winner === false && allShipsPlaced === false && currentShipIdx === 1) {
+    messageEl.textContent = `Please place ship ${ship}!`
+  }
   //   messageEl.textContent =  `Congratulations Player 1 won!`
   // } else {
-  //   messageEl.textContent =  `Congratulations Player 2 won!`
+  //   messageEl.textContent =  `Congratulations Computer won!`
   // }
 }
 
 
-
-
-//this event listener will only be added once the button is clicked
-//will need to remove it after all of the ships are placed for both players
-//then we will add a new event listener
-function handleBtnClick(evt){
-  if (evt.target.id === 'ship1') {
-    //if ship is already placed, return;
-    //if ship is already placed, hide button
-    //placeShip1()
-  }
-  squareEls.forEach(ele => {
-    ele.addEventListener("click", handleSqClick);
-  })
 }
 
 function handleSqClick(evt){
@@ -406,20 +378,32 @@ function handleSqClick(evt){
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       if (sqIdx === squareEls[i].id) {
-        if (board[row][col] === 1 || board[row][col] === 2 || board[row][col] === 3 || board[row][col] === 4 || board[row][col] === 5 || allShipsPlaced) {
-          return;
-        } 
+        if (winner === true) return
+        //no winner yet
+        //want to return out if allships are not placed yet (in board 'setting' state) and click on a square corresponding to a placed ship
+        if (!allShipsPlaced && (board[row][col] === 1 || board[row][col] === 2 || board[row][col] === 3 || board[row][col] === 4 || board[row][col] === 5)) return;
+        //otherwise either clicked square in game 'play' state, or clicked empty square in 'setting' state
         rowClicked = row;
         colClicked = col;
-       }
-       i++
+      }
+      i++
     }
   }
-  //if spot is occupied by a ship, or game has a winner, return out of function.
+  //after this loop, we want to check for any occurance in which we are still placing ships (every possible board state), we want to make sure all the pieces are placed in the loop
+  //after loop, 
+  if (allShipsPlaced === false) {
+  //if board is still being set, we want click to place ships according to logic
 
-  //check if the game is in placement state, 
 
-//   //after this loop this means there was no winner, and the current spot clicked is not occupied
+
+
+    //at the end of this we want to check if all ships placed !!!!!!!!
+  } else {
+    //we want to play the game, and change board appearance accordingly
+    hideShips()
+    aiPlaceShips()
+    play()
+  }
 //   placePiece(rowClicked, colClicked);
 //   checkBoardFull();
 //   switchPlayerTurn();
@@ -443,6 +427,21 @@ function handleSqClick(evt){
 //   }
 // }
 
-init();
+function hideShips(){
+  //if all ships are placed, and ships are not hidden, hide the ships
+  if (allShipsPlaced && !shipsHidden) {  
+    //change display of each box to be empty
+    //change shipsHidden to true so we don't use this function anymore
+    shipsHidden = true;
+  } 
+  //change display of each box to be empty
+  
+}
 
-// console.log(board)
+function aiPlaceShips(){
+
+}
+
+function play(){
+
+}
